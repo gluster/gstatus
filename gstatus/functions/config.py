6368d267674394022e49cf6bc8a29098ff7f8cf8
Paul Cuzner <pcuzner@redhat.com>
2015-01-13 08:43:47 +1300

    add default for gluster command timeouts

--- gstatus/functions/config.py
+++ gstatus/functions/config.py
@@ -30,11 +30,13 @@ def init():
 	global min_version
 	global snapshot_support
 	global no_progress_msgs
+	global CMD_TIMEOUT
 		
 	debug = False
 	min_version = 3.4
 	snapshot_support = 3.6
 	no_progress_msgs = False
+	CMD_TIMEOUT = 5
 
 	
 
